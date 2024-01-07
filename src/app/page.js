f6fd16d8e0a40991ae48f88c84e628e12f272268
bfranceschin <bfranceschin@gmail.com>
2024-01-06 21:53:30 -0300

    feat - fetch data from route

--- src/app/page.js
+++ src/app/page.js
@@ -6,44 +6,41 @@ export default function Home() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
+    // Function to fetch data from the API
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch('/api');
+        const result = await response.json();
+        console.log(result)
+        setData(result.data);
+        setLoading(false);
+      } catch (err) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
     useEffect(() => {
-        // const fetchData = async () => {
-        //     try {
-        //         setLoading(true);
-        //         const response = await fetch('/api/getImage');
-        //         const result = await response.json();
-        //         setData(result);
-        //         setLoading(false);
-        //     } catch (err) {
-        //         setError(true);
-        //         setLoading(false);
-        //     }
-        // };
-
-        // fetchData();
-
-        // // Set up a timer to fetch new data every 5 minutes (300000 milliseconds)
-        // const intervalId = setInterval(fetchData, 300000);
-
-        // // Clean up the interval on component unmount
-        // return () => clearInterval(intervalId);
-      const result = {
-        imageUrl: 'https://litoralguia.com.br/wordpress/wp-content/files/litoralguia.com.br/2021/12/imagem1.png',
-        text: 'imagem aleatória de saquarema'
-      };
-      setData(result);
-      setLoading(false);
+      fetchData();
+      const intervalId = setInterval(fetchData, 300000); // Fetch new data every 5 minutes
+      return () => clearInterval(intervalId);
     }, []);
 
-    // if (loading) return <p>Loading...</p>;
-    // if (error) return <p>Error loading data</p>;
+    // Button click handler to manually trigger data refresh
+    const handleRegenerateClick = () => {
+        fetchData();
+    };
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error loading data</p>;
 
     return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
           <img src={data.imageUrl} alt="Generated Image" className="max-w-sm rounded-lg shadow-lg" />
           <p className="mt-5 text-lg text-gray-700">{data.text}</p>
           <button
-            // onClick={handleRegenerateClick}
+            onClick={handleRegenerateClick}
             className="mt-5 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
           >
             Regenerate
