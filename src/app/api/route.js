be7e7c5b51a56897f19c900e63f93e9e0e85c962
bfranceschin <bfranceschin@gmail.com>
2024-01-06 23:06:48 -0300

    feat - dall-e-2 image from openai api

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,18 +1,25 @@
 
+import { ClientOptions, OpenAI } from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
+
 export async function GET() {
-  // const res = await fetch('https://data.mongodb-api.com/...', {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'API-Key': process.env.DATA_API_KEY,
-  //   },
-  // })
-  // const data = await res.json()
+  const res = await await openai.images.generate({
+    model: "dall-e-2",
+    prompt: "a white siamese cat",
+    n: 1,
+    size: "1024x1024",
+  });
+  console.log(res)
 
   const now = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
   const result = {
-    imageUrl: 'https://litoralguia.com.br/wordpress/wp-content/files/litoralguia.com.br/2021/12/imagem1.png',
-    text: now + ' imagem aleatória de saquarema '
+    // imageUrl: 'https://litoralguia.com.br/wordpress/wp-content/files/litoralguia.com.br/2021/12/imagem1.png',
+    imageUrl: res.data[0].url,
+    text: now + ' a white siamese cat '
   };
   // Directly return the result as a JSON response
   return new Response(JSON.stringify({ data: result }), {
