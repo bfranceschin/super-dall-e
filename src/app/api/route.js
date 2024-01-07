70210757de7f15341406c24036edd1fa7c46e9d6
bfranceschin <bfranceschin@gmail.com>
2024-01-07 00:48:39 -0300

    feat - changed prompt

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -5,21 +5,24 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_KEY,
 });
 
+const prompt = 'saquarema'
 export async function GET() {
   const res = await await openai.images.generate({
     model: "dall-e-2",
-    prompt: "a white siamese cat",
+    // model: "dall-e-3",
+    // prompt: "a white siamese cat",
+    prompt: prompt,
     n: 1,
     size: "1024x1024",
   });
-  console.log(res)
+  // console.log(res)
 
   const now = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
   const result = {
     // imageUrl: 'https://litoralguia.com.br/wordpress/wp-content/files/litoralguia.com.br/2021/12/imagem1.png',
     imageUrl: res.data[0].url,
-    text: now + ' a white siamese cat '
+    text: now + ' ' + prompt
   };
   // Directly return the result as a JSON response
   return new Response(JSON.stringify({ data: result }), {
